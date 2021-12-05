d7582dfedde7a3d4e4d2c1cf3df33e90fad00ed5
pohnpawit <pohnpawit@livingmobile.me>
2021-12-05 20:24:31 +0700

    wip : add edit test search

--- app/src/main/java/com/arnoract/piggiplan/MyApplication.kt
+++ app/src/main/java/com/arnoract/piggiplan/MyApplication.kt
@@ -2,6 +2,7 @@ package com.arnoract.piggiplan
 
 import android.app.Application
 import com.arnoract.piggiplan.di.AppComponent
+import com.arnoract.piggiplan.ui.di.UiComponent
 import com.facebook.drawee.backends.pipeline.Fresco
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -14,6 +15,7 @@ class MyApplication : Application() {
             androidContext(this@MyApplication)
         }
         AppComponent.init()
+        UiComponent.init()
         Fresco.initialize(this)
     }
 }
