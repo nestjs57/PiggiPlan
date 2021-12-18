e956b27a2050fc6541b5f77c2d56f43e71fec689
pohnpawit.n <pohnpawit.n@gmail.com>
2021-12-18 23:33:25 +0700

    first init send id to editFriend

--- app/src/main/java/com/arnoract/piggiplan/ui/di/CreateModule.kt
+++ app/src/main/java/com/arnoract/piggiplan/ui/di/CreateModule.kt
@@ -1,9 +1,13 @@
 package com.arnoract.piggiplan.ui.di
 
+import com.arnoract.piggiplan.ui.create.EditFriendViewModel
 import com.arnoract.piggiplan.ui.create.SelectAddressViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val createModule = module {
     viewModel { SelectAddressViewModel() }
+    viewModel { (userId: Long) ->
+        EditFriendViewModel(userId)
+    }
 }
