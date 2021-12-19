6213ef57c82f0063e99e2244508ffed760dc7219
pohnpawit.n <pohnpawit.n@gmail.com>
2021-12-19 21:29:40 +0700

    first init validate save

--- app/src/main/java/com/arnoract/piggiplan/ui/create/EditFriendViewModel.kt
+++ app/src/main/java/com/arnoract/piggiplan/ui/create/EditFriendViewModel.kt
@@ -3,14 +3,23 @@ package com.arnoract.piggiplan.ui.create
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.arnoract.piggiplan.core.CoroutinesDispatcherProvider
+import com.arnoract.piggiplan.core.successOrThrow
+import com.arnoract.piggiplan.domain.create.ValidateFriendAddressUseCase
+import com.arnoract.piggiplan.domain.exception.InvalidFriendAddressException
 import com.arnoract.piggiplan.ui.create.model.mapper.EditFriendToUiFriendAddressMapper
 import com.google.android.gms.maps.model.LatLng
 import com.hadilq.liveevent.LiveEvent
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class EditFriendViewModel(
     private val userId: Long,
     private val createPartyViewModelDelegateImpl: CreatePartyViewModelDelegateImpl,
-    private val selectAddressViewModelDelegateImpl: SelectAddressViewModelDelegateImpl
+    private val selectAddressViewModelDelegateImpl: SelectAddressViewModelDelegateImpl,
+    private val validateFriendAddressUseCase: ValidateFriendAddressUseCase,
+    private val coroutinesDispatcherProvider: CoroutinesDispatcherProvider
 ) : ViewModel(), CreatePartyViewModelDelegate by createPartyViewModelDelegateImpl,
     SelectAddressViewModelDelegate by selectAddressViewModelDelegateImpl {
 
@@ -18,13 +27,21 @@ class EditFriendViewModel(
     val name: LiveData<String?>
         get() = _name
 
-    private val _isBlankNameEvent = LiveEvent<Unit>()
-    val isBlankNameEvent: LiveData<Unit>
-        get() = _isBlankNameEvent
-
     private val _saveFriendSuccessEvent = LiveEvent<Unit>()
     val saveFriendSuccessEvent: LiveData<Unit>
         get() = _saveFriendSuccessEvent
+
+    private val _isBlankFriendNameEvent = LiveEvent<Boolean>()
+    val isBlankFriendNameEvent: LiveData<Boolean>
+        get() = _isBlankFriendNameEvent
+
+    private val _isBlankAddressNameEvent = LiveEvent<Boolean>()
+    val isBlankAddressNameEvent: LiveData<Boolean>
+        get() = _isBlankAddressNameEvent
+
+    private val _saveFailExceptionEvent = LiveEvent<String>()
+    val saveFailExceptionEvent: LiveData<String>
+        get() = _saveFailExceptionEvent
 
     init {
         if (userId != 0L) {
@@ -42,21 +59,34 @@ class EditFriendViewModel(
     }
 
     fun save(name: String) {
-        if (name.isBlank()) {
-            _isBlankNameEvent.value = Unit
-        } else {
-            val params = EditFriendToUiFriendAddressMapper.Params(
-                name = name,
-                addressId = getAddressId() ?: "",
-                addressName = getAddressName() ?: "",
-                latLng = getLatLng() ?: LatLng(0.0, 0.0)
-            )
-            if (userId == 0L) {
-                addFriend(EditFriendToUiFriendAddressMapper.map(params))
-            } else {
-                editFriend(userId, EditFriendToUiFriendAddressMapper.map(params))
+        viewModelScope.launch {
+            try {
+                withContext(coroutinesDispatcherProvider.io) {
+                    validateFriendAddressUseCase.invoke(
+                        ValidateFriendAddressUseCase.Params(
+                            name = name,
+                            addressName = getAddressName() ?: ""
+                        )
+                    ).successOrThrow()
+                }
+                val params = EditFriendToUiFriendAddressMapper.Params(
+                    name = name,
+                    addressId = getAddressId() ?: "",
+                    addressName = getAddressName() ?: "",
+                    latLng = getLatLng() ?: LatLng(0.0, 0.0)
+                )
+                if (userId == 0L) {
+                    addFriend(EditFriendToUiFriendAddressMapper.map(params))
+                } else {
+                    editFriend(userId, EditFriendToUiFriendAddressMapper.map(params))
+                }
+                _saveFriendSuccessEvent.value = Unit
+            } catch (e: InvalidFriendAddressException) {
+                _isBlankFriendNameEvent.value = e.isBlankFriendName
+                _isBlankAddressNameEvent.value = e.isBlankAddressName
+            } catch (e: Exception) {
+                _saveFailExceptionEvent.value = e.message ?: "Unknown Error"
             }
-            _saveFriendSuccessEvent.value = Unit
         }
     }
 }
